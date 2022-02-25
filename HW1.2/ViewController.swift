03e13f436578a2e9ce3929b4a7f3a49e3390843a
iReek <i_r_e_k@mail.ru>
2022-02-25 16:25:28 +0300

    HW1.2final

--- HW1.2/ViewController.swift
+++ HW1.2/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button: UIButton!
+    @IBOutlet var redCircle: UIView!
+    @IBOutlet var yellowCircle: UIView!
+    @IBOutlet var greenCircle: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 15
+        redCircle.alpha = 0.3
+        yellowCircle.alpha = 0.3
+        greenCircle.alpha = 0.3
     }
 
-
+    @IBAction func buttonPressed() {
+        if redCircle.alpha < 1 && yellowCircle.alpha < 1 {
+            redCircle.alpha = 1
+            yellowCircle.alpha = 0.3
+            greenCircle.alpha = 0.3
+        } else if yellowCircle.alpha < 1 && greenCircle.alpha < 1 {
+            redCircle.alpha = 0.3
+            yellowCircle.alpha = 1
+            greenCircle.alpha = 0.3
+        } else {
+            redCircle.alpha = 0.3
+            yellowCircle.alpha = 0.3
+            greenCircle.alpha = 1
+        }
+        button.setTitle("Next", for: .normal)
+    }
 }
 
