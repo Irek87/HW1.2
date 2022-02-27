22754a4776a41427d8911fd356bac124afdd5fe5
iReek <i_r_e_k@mail.ru>
2022-02-27 15:38:26 +0300

    HW1.2finalPoPrepodu

--- HW1.2/ViewController.swift
+++ HW1.2/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
 class ViewController: UIViewController {
 
     @IBOutlet var button: UIButton!
@@ -14,29 +17,48 @@ class ViewController: UIViewController {
     @IBOutlet var yellowCircle: UIView!
     @IBOutlet var greenCircle: UIView!
     
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.layer.cornerRadius = 15
-        redCircle.alpha = 0.3
-        yellowCircle.alpha = 0.3
-        greenCircle.alpha = 0.3
+        
+        button.layer.cornerRadius = 10
+        
+        redCircle.alpha = lightIsOff
+        yellowCircle.alpha = lightIsOff
+        greenCircle.alpha = lightIsOff
     }
 
-    @IBAction func buttonPressed() {
-        if redCircle.alpha < 1 && yellowCircle.alpha < 1 {
-            redCircle.alpha = 1
-            yellowCircle.alpha = 0.3
-            greenCircle.alpha = 0.3
-        } else if yellowCircle.alpha < 1 && greenCircle.alpha < 1 {
-            redCircle.alpha = 0.3
-            yellowCircle.alpha = 1
-            greenCircle.alpha = 0.3
-        } else {
-            redCircle.alpha = 0.3
-            yellowCircle.alpha = 0.3
-            greenCircle.alpha = 1
-        }
-        button.setTitle("Next", for: .normal)
+    override func viewWillLayoutSubviews() {
+        redCircle.layer.cornerRadius = redCircle.frame.width / 2
+        yellowCircle.layer.cornerRadius = yellowCircle.frame.width / 2
+        greenCircle.layer.cornerRadius = greenCircle.frame.width / 2
     }
+    
+    @IBAction func buttonPressed() {
+//        if button.currentTitle == "START" {
+//            button.setTitle("NEXT", for: .normal)
+//        }
+        
+        button.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenCircle.alpha = lightIsOff
+            redCircle.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redCircle.alpha = lightIsOff
+            yellowCircle.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenCircle.alpha = lightIsOn
+            yellowCircle.alpha = lightIsOff
+            currentLight = .red
+        }
+        
+        }
 }
 
